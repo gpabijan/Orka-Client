45c7c1c8c73fd8f3af1f70a8a78b2851b14b6177
GPabijan <gpabij@gmail.com>
2019-02-22 13:25:52 +0100

    new version + some fixes

--- client.js
+++ client.js
@@ -1,38 +1,40 @@
 
-// const app = require('express')()
+ const app = require('express')();
 'use strict'
 const piClient = require('./piClient.js')
-const tty = require('tty.js')
+const http = require('http');
+
 let client = null
 const io = require('socket.io')
 
 const port = process.argv.length > 2 ? process.argv[2] : 1993
 
-const cc = tty.createServer({ shell: 'bash', port: port, localOnly: false })
+ const cc = http.createServer({ shell: 'bash', users: {}, port: port })
+
 const cloudcmd = require('cloudcmd')
 const socket = io.listen(cc, {
   path: '/cloud/socket.io'
 })
 
-cc.get('/cloud/*', cloudcmd({
+app.get('/cloud/*', cloudcmd({
   socket: socket,
   config: {
     prefix: '/cloud'
   }
 }))
-cc.put('/cloud/*', cloudcmd({
+app.put('/cloud/*', cloudcmd({
   socket: socket,
   config: {
     prefix: '/cloud'
   }
 }))
-cc.delete('/cloud/*', cloudcmd({
+app.delete('/cloud/*', cloudcmd({
   socket: socket,
   config: {
     prefix: '/cloud'
   }
 }))
-cc.post('/connect', function (req, res) {
+app.post('/connect', function (req, res) {
   let data = ''
   req.on('data', (chunk) => { data += chunk })
   req.on('end', () => {
@@ -45,5 +47,7 @@ cc.post('/connect', function (req, res) {
     client.start()
   })
 })
+
+
 console.log('Orka Client is listening on port ' + port)
 cc.listen(port)
